f31da3717724234f4934f9273b081b09b98c6cc1
Alesh Houdek <mail@alesh.com>
2019-02-02 06:42:27 -0700

    importing ddc index as array of objects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+// import customData from './data/ddcIndex.json';
 
 class App extends Component {
+
   render() {
+    const ddc = require('./data/ddcIndex.json');
+    console.log(ddc[0]);
+
     return (
       <div className="App">
-      App component
+        {ddc[0].number + ' ' + ddc[0].description}
       </div>
     );
   }
