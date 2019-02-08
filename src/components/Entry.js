96982e68ded69ff2ea266e5fc469de2c4f05f7f7
Alesh Houdek <mail@alesh.com>
2019-02-08 03:19:48 -0700

    Chevrons between path elements!

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Home, ChevronsRight } from 'react-feather'
+import { Home, ChevronsRight, ChevronRight } from 'react-feather'
 
 function Entry ({ entry, showHome, currentLocation, handleClick }) {
   const item = entry[entry.length - 1]
@@ -10,23 +10,32 @@ function Entry ({ entry, showHome, currentLocation, handleClick }) {
       { (showHome || (path.length > 0)) ? (
         <p className='result-path'>
           { showHome ? (
-            <span
-              key='home'
-              onClick={() => handleClick('xxx')}
-              className='clickable home'
-            >
-              <Home />
-            </span>
+            <React.Fragment>
+              <span
+                key='home'
+                onClick={() => handleClick('xxx')}
+                className='clickable home'
+              >
+                <Home />
+              </span>
+              <ChevronRight className='path-separator' />
+            </React.Fragment>
           ) : null
           }
-          {path.map(item => (
-            <span
-              key={item.id}
-              onClick={() => handleClick(item.id)}
-              className={(item.id !== currentLocation) ? 'clickable' : ''}
-            >
-              {entryToString(item)}
-            </span>
+          {path.map((item, i, arr) => (
+            <React.Fragment>
+              <span
+                key={item.id}
+                onClick={() => handleClick(item.id)}
+                className={(item.id !== currentLocation) ? 'clickable' : ''}
+              >
+                {entryToString(item)}
+              </span>
+              { (i !== arr.length - 1) ?
+                <ChevronRight className='path-separator' /> :
+                ''
+              }
+            </React.Fragment>
           ))}
         </p>
       ) : null }
@@ -39,7 +48,7 @@ function Entry ({ entry, showHome, currentLocation, handleClick }) {
         className={item.subordinates ? 'result clickable' : 'result'}
       >
         {entryToString(item)}
-        {item.subordinates ? < ChevronsRight /> : ''}
+        {item.subordinates ? <ChevronsRight /> : ''}
       </p>
     </div>
   )
