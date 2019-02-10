6e6c9c77c7e16c208c62dcbed7af17d39a8a80cb
Alesh Houdek <mail@alesh.com>
2019-02-10 14:18:26 -0700

    Added brand

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { Home, ChevronLeft, ChevronRight } from 'react-feather'
 function Search ({ searchTerm, handleSearch }) {
   return (
     <div className='navbar'>
+      <div className='brand'>Wayfarer</div>
       <div className='button'><Home /></div>
       <div className='button'><ChevronLeft /></div>
       <div className='button'><ChevronRight /></div>
